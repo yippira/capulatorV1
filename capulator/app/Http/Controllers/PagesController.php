da447dc55b35b7bfe742f367ef513ba15955d287
pereira yip <yippygames@gmail.com>
2018-06-30 02:46:42 +0800

    added basic layout

---
+++ capulator/app/Http/Controllers/PagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+
+        //return main landing page
+        
+        return view('pages.index');
+
+    }
+}
---
+++ capulator/resources/views/pages/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>This is the CAP calculator you've been waiting for!</h1>
+@endsection
