4fd6e484c189eccb528efec3ae9b61754b80ec5f
lolhaha95 <bryan_wang19@hotmail.com>
2018-07-27 01:50:49 +0800

    Lines starting Remove some parts thats all

--- capulator/resources/views/pages/index.blade.php
+++ capulator/resources/views/pages/index.blade.php
@@ -93,54 +93,27 @@
 
     <body class="color-custom style-simple button-flat layout-full-width if-zoom if-border-hide no-content-padding no-shadows header-classic minimalist-header-no sticky-header sticky-tb-color ab-hide subheader-both-center menu-link-color menuo-right menuo-no-borders mobile-tb-center mobile-mini-mr-ll tablet-sticky mobile-header-mini mobile-sticky be-reg-20881">
             <div id="Wrapper">
-                <div id="Header_wrapper">
-                    <header id="Header">
-                        <div class="header_placeholder"></div>
-                        <div id="Top_bar">
-                            <div class="container">
-                                <div class="column one">
-                                    <div class="top_bar_left clearfix">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </header>
-                </div>
+
                 <div id="Content">
                     <div class="content_wrapper clearfix">
                         <div class="sections_group">
                             <div class="entry-content">
-                                <div class="section mcb-section" style="padding-top:125px; padding-bottom:225px; background-color:#FFFFFF">
+                                <div class="section mcb-section" style="padding-top:50px; padding-bottom:120px; background-color:#FFFFFF">
                                     {{-- <div class="section-decoration top" style="background-image:url(images/1.jpg);height:20px"></div> --}}
-                                    <div class="section_wrapper mcb-section-inner">
-                                        <div class="wrap mcb-wrap one valign-top clearfix">
-                                            <div class="mcbH-wrap-inner">
-                                                <div class="column mcb-column one column_column">
-                                                    <div class="column_attr clearfix">
-                                                        <h3 style="color:#b8ad00">What we believe in</h3>
-                                                        <h1>Less is more
-                                                            <br> Help me Help you
-                                                            <br> GG LA
-                                                        </h1>
-                                                    </div>
-                                                </div>
-                                                <div class="column mcb-column one-third column_button">
-                                                    <a class="button  button_right button_size_3 button_js" href="" style=" background-color:#fff !important; color:#000"><span class="button_icon"><i class="icon-docs" style=" color:#000 !important"></i></span><span class="button_label">See our work</span></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    
                                     <div class="section-decoration bottom" style="height:51px;"></div>
                                 </div>
+                                {{-- To make picture bigger --}}
                                 <div class="section mcb-section equal-height" style="padding-top:0px; padding-bottom:100px">
                                     <div class="section_wrapper mcb-section-inner">
                                         <div class="wrap mcb-wrap one valign-top move-up clearfix" style="margin-top:-120px">
-                                            <div class="mcb-wrap-inner">
+                                            <div class="mcb-wrap-inner"> 
+
                                                 <div class="column mcb-column one-third column_zoom_box ">
                                                     <div class="zoom_box">
                                                         <a href="content/simple2/work-details.html">
                                                             <div class="photo">
-                                                                    <img src="{{ URL::to('/') }}/images/1.jpg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+                                                                    <img src="{{ URL::to('/') }}/images/Analytics.jpg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
                                                                     <!-- LAYERS -->
                                                             </div>
                                                             <!-- LAYER NR. 1 -->
@@ -151,7 +124,7 @@
                                                             <div class="desc" style="background-color:rgba(0, 0, rgba(255, 4, 55, 0.8), 0.8)">
                                                                 <div class="desc_wrap">
                                                                     <div class="desc_txt">
-                                                                        1st pic
+                                                                        Analytics
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -162,28 +135,12 @@
                                                     <div class="zoom_box">
                                                         <a href="">
                                                             <div class="photo">
-                                                                    <img src="{{ URL::to('/') }}/images/1.jpg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+                                                                    <img src="{{ URL::to('/') }}/images/Planning.jpg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
                                                             </div>
                                                             <div class="desc" style="background-color:rgba(255, 4, 55, 0.8)">
                                                                 <div class="desc_wrap">
                                                                     <div class="desc_txt">
-                                                                        lel
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                                <div class="column mcb-column one-third column_zoom_box ">
-                                                    <div class="zoom_box">
-                                                        <a href="">
-                                                            <div class="photo">
-                                                                    <img src="{{ URL::to('/') }}/images/1.jpg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
-                                                            </div>
-                                                            <div class="desc" style="background-color:rgba(228, 157, 51, 0.8)">
-                                                                <div class="desc_wrap">
-                                                                    <div class="desc_txt">
-                                                                        Ignissimos eius quaerat
+                                                                        Plan Your Future
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -194,28 +151,12 @@
                                                     <div class="zoom_box">
                                                         <a href="content/simple2/work-details.html">
                                                             <div class="photo">
-                                                                    <img src="{{ URL::to('/') }}/images/1.jpg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+                                                                    <img src="{{ URL::to('/') }}/images/Calculate.jpeg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
                                                             </div>
                                                             <div class="desc" style="background-color:rgba(57, 97, 143, 0.8)">
                                                                 <div class="desc_wrap">
                                                                     <div class="desc_txt">
-                                                                        Blanditiis voluptas voluptates
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                                <div class="column mcb-column one-third column_zoom_box ">
-                                                    <div class="zoom_box">
-                                                        <a href="content/simple2/work-details.html">
-                                                            <div class="photo">
-                                                                    <img src="{{ URL::to('/') }}/images/1.jpg" alt="" data-bgposition="center bottom" data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
-                                                            </div>
-                                                            <div class="desc" style="background-color:rgba(0, 0, 0, 0.8)">
-                                                                <div class="desc_wrap">
-                                                                    <div class="desc_txt">
-                                                                        Blanditiis voluptas voluptates
+                                                                        Calculate Your CAP
                                                                     </div>
                                                                 </div>
                                                             </div>
